7d944cfb7028d306c3ce4fa1abf69db3f6070c08
Courtney Sands <cdsands5@gmail.com>
2022-04-29 14:03:42 -0400

    inital commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis nisi, tempore dicta odit mollitia sequi ducimus beatae repellendus voluptas sunt aliquid dolore asperiores nihil eveniet impedit quisquam voluptates eos iusto magni temporibus at explicabo sed enim! Harum hic veniam esse.
+        </p>
+    </section>
+  )
+}
+
+export default About
